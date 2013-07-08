203ac8584ecb0b20d49688599d998345dc391a8c
Nathan Broadbent <nathan.f77@gmail.com>
2013-07-08 14:29:26 +1200

    retry failed requests

--- barcode_instructions/lib/microwave_cooking_db.rb
+++ barcode_instructions/lib/microwave_cooking_db.rb
@@ -7,14 +7,18 @@ class MicrowaveCookingDB
   end
 
   def find(upc)
-    begin
-      puts "Looking up #{upc} on microwavecookingdb.com..."
-      product = HTTParty.get("http://www.microwavecookingdb.com/products/#{upc}.json", query: {email: @email, api_key: @api_key})
-      return nil if product["error"]
-      product
-    rescue Exception => ex
-      p $!, *$@
-      nil
+    puts "Looking up #{upc} on microwavecookingdb.com..."
+
+    # Retry failed request 3 times
+    3.times do
+      begin
+        product = HTTParty.get("http://www.microwavecookingdb.com/products/#{upc}.json", query: {email: @email, api_key: @api_key})
+        return nil if product["error"]
+        return product
+      rescue Exception => ex
+        p $!, *$@
+        nil
+      end
     end
   end
 end
